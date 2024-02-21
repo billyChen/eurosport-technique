22f00187ba0033deac4431f501d8d1f99be0f297
Billy <billy.chen@semana.io>
2024-02-21 17:50:25 +0100

    create FlipCard component

---
+++ app/ui/flip-card/FlipCard.tsx
@@ -0,0 +1,47 @@
+import classnames from "classnames";
+import React from "react";
+
+type FlipCardProps = React.HTMLAttributes<HTMLDivElement> & {
+  isFlipped: boolean;
+  backCard: React.ReactNode;
+  frontCard: React.ReactNode;
+  onValueChange: () => void;
+};
+
+export const FlipCard = ({
+  className,
+  isFlipped,
+  backCard,
+  frontCard,
+  onValueChange,
+  ...props
+}: FlipCardProps) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.key === "Enter") {
+      onValueChange();
+    }
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+
+    onValueChange();
+  };
+
+  return (
+    <section
+      {...props}
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleKeyDown} 
+      aria-pressed={isFlipped}
+      onClick={handleClick}
+      className={classnames(
+        "flex w-full flex-col justify-center items-center border",
+        className,
+      )}
+    >
+      <div className={"w-full h-fit"}>{isFlipped ? backCard : frontCard}</div>
+    </section>
+  );
+};
---
+++ app/ui/flip-card/__tests__/FlipCard.test.tsx
@@ -0,0 +1,40 @@
+import { FlipCard } from "@/app/ui/flip-card/FlipCard";
+import { render, screen } from "@testing-library/react";
+
+describe("FlipCard", () => {
+  it("should render the front card", async () => {
+    render(
+      <FlipCard
+        isFlipped={false}
+        onValueChange={() => {}}
+        frontCard={<div>This is the front card</div>}
+        backCard={<div>This is the back card</div>}
+      />,
+    );
+
+    expect(
+      await screen.queryByText("This is the front card"),
+    ).toBeInTheDocument();
+    expect(
+      await screen.queryByText("This is the back card"),
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render the back card", async () => {
+    render(
+      <FlipCard
+        isFlipped={true}
+        onValueChange={() => {}}
+        frontCard={<div>This is the front card</div>}
+        backCard={<div>This is the back card</div>}
+      />,
+    );
+
+    expect(
+      await screen.queryByText("This is the front card"),
+    ).not.toBeInTheDocument();
+    expect(
+      await screen.queryByText("This is the back card"),
+    ).toBeInTheDocument();
+  });
+});
