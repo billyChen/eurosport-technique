b7fc1dc485661ac318dd5bf23c60674df036ced5
Billy <billy.chen@semana.io>
2024-02-21 18:32:41 +0100

    Move schema graphql to relay folder

--- relay.config.js
+++ relay.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    src: "./",
-    language: "typescript",
-    schema: "./data/schema.graphql",
-    exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
-  };
-  
+  src: "./",
+  language: "typescript",
+  schema: "./relay/schema.graphql",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+};
